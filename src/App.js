c0c52e3369d47e860314f77e8ed995b27e23141d
Jiseung Roh <jiseung.roh@gmail.com>
2018-03-12 08:14:25 +0900

    Implement SectionCreator

--- src/App.js
+++ src/App.js
@@ -1,21 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import AppLayout from './components/layout/AppLayout'
+import SectionCreator from './components/SectionCreator'
+import './styles/App.css'
+import ColorPaletteSection from './components/sections/ColorPaletteSection'
+import TypographySection from './components/sections/TypographySection'
+import TextareaSection from './components/sections/TextareaSection'
 
-class App extends Component {
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      sections: [],
+    }
+  }
+
+  addSection = (sectionMode) => {
+    this.setState({
+      sections: [
+        ...this.state.sections,
+        {
+          mode: sectionMode,
+          id: Date.now(),
+        },
+      ],
+    })
+  }
+
+  deleteSection = (index) => {
+    const newSections = [
+      ...this.state.sections,
+    ]
+
+    newSections.splice(index, 1)
+
+    this.setState({
+      sections: newSections,
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <AppLayout>
+
+        <header className='page__header'>
+          <h1 className='page__title'>
+            Name of the styleguide
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+        <section>
+          {
+            this.state.sections.map((section, index) => {
+              switch (section.mode) {
+                case 'color-palette':
+                  return <ColorPaletteSection
+                    key={index}
+                    deleteSection={this.deleteSection}
+                    index={index}
+                    />
+                case 'typography':
+                  return <TypographySection
+                    key={index}
+                    deleteSection={this.deleteSection}
+                    index={index}
+                    />
+                case 'textarea':
+                  return <TextareaSection
+                    key={index}
+                    deleteSection={this.deleteSection}
+                    index={index}
+                    />
+              }
+            })
+          }
+          <SectionCreator addSection={this.addSection} />
+        </section>
+
+      </AppLayout>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/SectionCreator.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import '../styles/SectionCreator.css'
+import * as ColorPaletteIcon from '../assets/images/icon-color-palette.svg'
+import * as TypographyIcon from '../assets/images/icon-typography.svg'
+import * as MoreIcon from '../assets/images/icon-more.svg'
+
+class SectionCreator extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      sectionMode: null,
+    }
+  }
+
+  setColorPaletteBlock = () => {
+    this.props.addSection('color-palette')
+  }
+
+  setTypographyBlock = () => {
+    this.props.addSection('typography')
+  }
+
+  setTextAreaBlock = () => {
+    this.props.addSection('textarea')
+  }
+
+  render () {
+    return (
+      <section className='section-creator'>
+        {
+          this.state.sectionMode === null &&
+          <React.Fragment>
+            <button
+              type='button'
+              onClick={this.setColorPaletteBlock}
+            >
+              <img
+                src={ColorPaletteIcon}
+                alt='Color Palette'
+              />
+            </button>
+            <button
+              type='button'
+              onClick={this.setTypographyBlock}
+            >
+              <img
+                src={TypographyIcon}
+                alt='Typography'
+              />
+            </button>
+            <button
+              type='button'
+              onClick={this.setTextAreaBlock}
+            >
+              <img
+                src={MoreIcon}
+                alt='More'
+              />
+            </button>
+          </React.Fragment>
+        }
+      </section>
+    )
+  }
+}
+
+export default SectionCreator
